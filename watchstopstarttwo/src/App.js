7b1363ef655e1cf7212282e7ccaa2fc9d13f6625
Abhishek M <abhishekmaldikar@gmail.com>
2024-03-25 22:09:20 +0530

    final

--- watchstopstarttwo/src/App.js
+++ watchstopstarttwo/src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState, useEffect } from "react";
 
 function App() {
+
+  const [time, settime] = useState(0);
+  const [state, setState] = useState(true);
+
+  useEffect(() => {
+    if(!state){
+      window.interval = setInterval(() => {settime((prev) => prev + 1000)},1000);
+    }else{
+      clearInterval(window.interval);
+    }
+    return () => clearInterval(window.interval);
+  }, [state]);
+
+  function clearTimeOut() {
+    clearInterval(window.interval);
+    settime(0);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+      <h1>Stop watch</h1>
+      <p style={{ display: "inline" }}>Time : </p>
+      <span>{Math.floor(time / 60000) % 60}</span>:<span>{Math.floor(time / 1000) % 60}</span>
+      <br />
+      {state ? (
+        <button style={{marginRight : "4px"}} onClick={() => setState(false)}>start</button>
+      ) : (
+        <button style={{marginRight : "4px"}} onClick={() => setState(true)}>stop</button>
+      )}
+      <button onClick={() => clearTimeOut()}>reset</button>
+      </div>
     </div>
   );
 }
